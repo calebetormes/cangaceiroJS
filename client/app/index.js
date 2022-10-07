0353c00dd958b4b10bf5e1f847e5f2be0c6c0b38
calebetormes <calebetormes@gmail.com>
2022-10-06 21:56:08 -0300

    tt

--- client/app/index.js
+++ client/app/index.js
@@ -3,4 +3,4 @@ var campos = [
     document.querySelector('#valor'),
     document.querySelector('#quantidade'),
 ];
-console.log(campos); //verificando conteudo do array
+console.log(campos); //verificando conteudo do array teste
