1a6b190b5a502076a25faca41357832918c35ffc
calebetormes <calebetormes@gmail.com>
2022-10-06 21:52:47 -0300

    first commit

---
+++ client/app/index.js
@@ -0,0 +1,6 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade'),
+];
+console.log(campos); //verificando conteudo do array
