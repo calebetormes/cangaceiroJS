9c0c4c076c319d8a7658c9485ad731b015127412
calebetormes <calebetormes@gmail.com>
2023-01-25 11:41:57 -0300

    Add submit ao form e exibe alerta 'oi'

--- client/app/index.js
+++ client/app/index.js
@@ -4,3 +4,13 @@ var campos = [
     document.querySelector('#quantidade'),
 ];
 console.log(campos); //verificando conteudo do array testedr
+
+/*seleciona o elemento "tbody" dentro da primeira tabela encontrada no 
+HTML e armazena em uma variável chamada "tbody"*/
+var tbody = document.querySelector('table tbody');
+
+// Adiciona um evento de submit ao primeiro formulário encontrado no HTML
+document.querySelector('.form').addEventListener('submit', function(event) {
+        alert('oi');// Mostra uma caixa de diálogo "oi" quando o formulário é enviado 
+    }
+);
